96cd383f1282f2ff6c488967aaadd7b63f00ef36
Joe Nelson <github@begriffsschrift.com>
2011-10-06 13:11:27 -0500

    fixed file selector, and added prompt for deleting content

--- source/js/types.js
+++ source/js/types.js
@@ -4,9 +4,11 @@ function typeOfFile(path) {
 		return 'unknown';
 	}
   	return {'png':'image', 'gif':'image', 'txt':'text', 'html':'text',
-               'jpg':'image', 'swf':'video', 'mpeg':'video', 'avi':'video', 'flv':'video'}[temp[1]] || 'unknown';
+                'jpg':'image', 'swf':'video', 'mpeg':'video', 'avi':'video',
+	        'flv':'video', 'mp3':'audio', 'wav':'audio', 'aiff':'audio'}[temp[1]] || 'unknown';
 }
 
 function iconForType(type) {
-	return {'image': "icons/image.png", 'text': "icons/text.png", 'video': 'icons/video.png'}[type] || 'unknown';
+	return {'image': "icons/image.png", 'text': "icons/text.png",
+	        'video': 'icons/video.png', 'audio': "icons/audio.png"}[type] || 'unknown';
 }
