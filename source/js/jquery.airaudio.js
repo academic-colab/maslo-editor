3de94c7a495aff17d577c56a07e0ce108e066cac
Joe Nelson <github@begriffsschrift.com>
2011-10-31 16:47:09 -0500

    wrote jquery plugin to do audio

---
+++ source/js/jquery.airaudio.js
@@ -0,0 +1,61 @@
+/*
+ *  Project: Adobe AIR Audio Player
+ *  Description: Renders an MP3 player
+ *  Author: Joe Nelson
+ *  License: MIT
+ *
+ *  Built on https://github.com/zenorocha/jquery-boilerplate
+ */
+
+;(function ( $, window, document, undefined ) {
+    
+    // Create the defaults once
+    var pluginName = 'AirAudio',
+        defaults = { };
+
+    // The actual plugin constructor
+    function AirAudio( element, options ) {
+        this.element = element;
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+    AirAudio.prototype.init = function () {
+      mp3     = new air.Sound(new air.URLRequest(this.options['file']));
+      channel = null;
+      btn     = $('<input type="button" value="Play" />');
+      $(this.element).append(btn);
+      toggleSound = function() {
+         if(channel) {
+            dlgCloseTrigger = function() {};
+            channel.stop();
+            channel = null;
+         } else {
+            channel = mp3.play();
+            channel.addEventListener(air.Event.SOUND_COMPLETE,
+            function(e) { channel = null; btn.attr('value', 'Play'); }); 
+            dlgCloseTrigger = function() {
+               channel.stop();
+               btn.attr('value', 'Play');
+            };
+         }
+         btn.attr('value', channel ? 'Stop' : 'Play');
+      }
+      btn.click(toggleSound)
+    };
+
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new AirAudio( this, options ));
+            }
+        });
+    }
+
+})(jQuery, window, document);
