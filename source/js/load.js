c391fa9e4598b14d864368fa2df40cd0f7769442
Joe Nelson <github@begriffsschrift.com>
2011-11-14 15:59:40 -0600

    moses and my refactoring

--- source/js/load.js
+++
@@ -1,8 +0,0 @@
-function loadTextfile(path) {
-	var f = new air.File(path);
-	var fs = new air.FileStream();
-	fs.open(f, air.FileMode.READ);
-	var ret = fs.readMultiByte(fs.bytesAvailable, "utf-8");
-	fs.close();
-	return ret;
-}
