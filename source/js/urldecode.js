5ca513955b7bc3fecd82c2d25ec3a1398ae9034c
Joe Nelson <github@begriffsschrift.com>
2011-12-05 14:18:44 -0600

    fixed url(en/de)coding in quiz, plus pathing

--- source/js/urldecode.js
+++ source/js/urldecode.js
@@ -1,38 +1,10 @@
-// from http://cass-hacks.com/articles/code/js_url_encode_decode/
+// overrides decode to set '+' to space, modern style
 function urldecode(encodedString) {
-  var output = encodedString;
-  var binVal, thisString;
-  var myregexp = /(%[^%]{2})/;
-  while ((match = myregexp.exec(output)) != null
-                     && match.length > 1
-                                  && match[1] != '') {
-      binVal = parseInt(match[1].substr(1),16);
-      thisString = String.fromCharCode(binVal);
-      output = output.replace(match[1], thisString);
-    }
-  return output;
+		return decodeURIComponent(encodedString.replace(/\+/g, ' '));
 }
 
-function urlencode (clearString) {
-  var output = '';
-  var x = 0;
-  clearString = clearString.toString();
-  var regex = /(^[a-zA-Z0-9_.]*)/;
-  while (x < clearString.length) {
-      var match = regex.exec(clearString.substr(x));
-      if (match != null && match.length > 1 && match[1] != '') {
-          output += match[1];
-            x += match[1].length;
-          } else  {
-                if (clearString[x] == ' ')
-                  output += '+';
-                else {
-                        var charCode = clearString.charCodeAt(x);
-                        var hexVal = charCode.toString(16);
-                        output += '%' + ( hexVal.length < 2 ? '0' : '' ) + hexVal.toUpperCase();
-                      }
-                x++;
-              }
-    }
-  return output;
+// no change from javascript default, but including for
+// naming parity
+function urlencode(clearString) {
+		return encodeURIComponent(clearString);
 }
