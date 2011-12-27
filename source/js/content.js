8d12f49fec92a03fec1e31d689ec0deaf1844326
Joe Nelson <github@begriffsschrift.com>
2011-12-27 11:10:12 -0600

    newest work on the content classes

---
+++ source/js/content.js
@@ -0,0 +1,13 @@
+function Content(path, title, desc) {
+	this.path = path;
+	this.title = title;
+	this.desc = desc;
+}
+
+Content.prototype.toString = function() {
+	
+}
+
+Content.prototype.save = function() {
+	
+}
